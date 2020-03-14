ad2048572885305eeccea2bfa7283904af32f452
sekwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-03-14 12:34:23 +0100

    Added filtering functionality for joins of two tables

--- lib/queries/two/filter_two_tables.ts
+++ lib/queries/two/filter_two_tables.ts
@@ -1,26 +1,21 @@
 import {Constructor, SelectStatement} from '../../select_statement'
-import {parseJoin} from '../../parsing/join_parsing'
-import {MapTwoTables} from './map_two_tables'
-import {SelectTwoTables} from './select_two_tables'
+import {parsePredicate} from '../../parsing/predicate_parsing'
 import {Value} from '../../column_operations'
-import {SortTable} from '../one/sort_table'
 import {SortTwoTables} from './sort_two_tables'
-import {FilterTwoTables} from './filter_two_tables'
+import {SelectTwoTables} from './select_two_tables'
+import {MapTwoTables} from './map_two_tables'
 
-export class JoinSecondTable<T1, T2, K1> {
+export class FilterTwoTables<T1, T2> {
     private readonly statement: SelectStatement
 
     constructor(
         private firstConstructor: Constructor<T1>,
         private secondConstructor: Constructor<T2>,
         existingStatement: SelectStatement,
-        secondTableName: string,
-        left: (firstTable: T1) => K1,
-        right: (secondTable: T2) => K1) {
-
+        predicate: (first: T1, second: T2) => boolean) {
         this.statement = {
             ...existingStatement,
-            joins: existingStatement.joins.concat([parseJoin(secondTableName, left, right)])
+            predicates: existingStatement.predicates.concat(parsePredicate(predicate))
         }
     }
 
@@ -36,12 +31,12 @@ export class JoinSecondTable<T1, T2, K1> {
         return new SortTwoTables(this.firstConstructor, this.secondConstructor, this.statement, sortBy, 'desc')
     }
 
+    select(first: string, second: string): SelectTwoTables<T1, T2> {
+        return new SelectTwoTables(this.firstConstructor, this.secondConstructor, this.statement, first, second)
+    }
+
     map<U>(f: (first: T1, second: T2) => U): MapTwoTables<T1, T2, U> {
         return new MapTwoTables(this.statement, f)
     }
 
-    select(first: string, second: string): SelectTwoTables<T1, T2> {
-        return new SelectTwoTables<T1, T2>(this.firstConstructor, this.secondConstructor, this.statement, first, second)
-    }
 }
-
---
+++ test/queries/two/filter_two_tables.test.ts
@@ -0,0 +1,55 @@
+import {departments, employees} from '../../test_tables'
+import {joinWithNewLine} from '../../../lib/parsing/javascript_parsing'
+import * as assert from 'assert'
+
+describe('Filtering a join of two tables works for a predicate', () => {
+
+    const join = employees
+        .join(departments, e => e.departmentId, d => d.id)
+
+    it('that applies to the first table', () => {
+        assert.equal(
+            join
+                .filter((e, d) => e.id === 1)
+                .map((e, d) => d.name)
+                .toSql(),
+            joinWithNewLine([
+                'SELECT t2.name',
+                'FROM employees t1',
+                'INNER JOIN departments t2 ON t1.department_id = t2.id',
+                'WHERE t1.id = 1'
+            ])
+        )
+    })
+
+    it('that applies to the second table', () => {
+        assert.equal(
+            join
+                .filter((e, d) => d.id === 1)
+                .map((e, d) => e.id)
+                .toSql(),
+            joinWithNewLine([
+                'SELECT t1.id',
+                'FROM employees t1',
+                'INNER JOIN departments t2 ON t1.department_id = t2.id',
+                'WHERE t2.id = 1'
+            ])
+        )
+    })
+
+    it('that applies to the both tables', () => {
+        assert.equal(
+            join
+                .filter((e, d) => e.lastName === 'Doe' && d.id === 1)
+                .map((e, d) => e.id === 1)
+                .toSql(),
+            joinWithNewLine([
+                'SELECT t1.id',
+                'FROM employees t1',
+                'INNER JOIN departments t2 ON t1.department_id = t2.id',
+                "WHERE t1.last_name = 'Doe' AND t2.id = 1"
+            ])
+        )
+    })
+
+})
