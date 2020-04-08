eeb6776000a8de876d856a82ce1b070f251e5e69
sekwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-04-08 20:33:39 +0200

    Fixed the signatures in GroupFourTables and SortGroupingOfFourTables.

--- lib/queries/four/group_four_tables.ts
+++ lib/queries/four/group_four_tables.ts
@@ -11,16 +11,16 @@ import {
 export class GroupFourTables<T1, T2, T3, T4, K extends ValueRecord> {
     constructor(private readonly statement: GroupSelectStatement) {}
 
-    sortBy(sortBy: (key: K, first: AggregatableTable<T1>, second: AggregatableTable<T2>, third: AggregatableTable<T3>, count: () => Count) => K|Max|Min|Avg|Sum|Count): SortGroupingOfFourTables<T1, T2, T3, T4, K> {
+    sortBy(sortBy: (key: K, first: AggregatableTable<T1>, second: AggregatableTable<T2>, third: AggregatableTable<T3>, fourth: AggregatableTable<T4>, count: () => Count) => K|Max|Min|Avg|Sum|Count): SortGroupingOfFourTables<T1, T2, T3, T4, K> {
         return new SortGroupingOfFourTables(addAscendingGroupOrder(this.statement, sortBy))
     }
 
-    sortDescendinglyBy(sortBy: (key: K, first: AggregatableTable<T1>, second: AggregatableTable<T2>, third: AggregatableTable<T3>, count: () => Count) => K|Max|Min|Avg|Sum|Count): SortGroupingOfFourTables<T1, T2, T3, T4, K> {
+    sortDescendinglyBy(sortBy: (key: K, first: AggregatableTable<T1>, second: AggregatableTable<T2>, third: AggregatableTable<T3>, fourth: AggregatableTable<T4>, count: () => Count) => K|Max|Min|Avg|Sum|Count): SortGroupingOfFourTables<T1, T2, T3, T4, K> {
         return new SortGroupingOfFourTables(addDescendinGroupOrder(this.statement, sortBy))
     }
 
     aggregate<A extends GroupAggregationRecord<K>>(
-        aggregation: (key: K, first: AggregatableTable<T1>, second: AggregatableTable<T2>, third: AggregatableTable<T3>) => EnforceNonEmptyRecord<A> & A): SelectRows<A> {
+        aggregation: (key: K, first: AggregatableTable<T1>, second: AggregatableTable<T2>, third: AggregatableTable<T3>, fourth: AggregatableTable<T4>) => EnforceNonEmptyRecord<A> & A): SelectRows<A> {
         return aggregateGroups(this.statement, aggregation)
     }
 }
--- lib/queries/four/sort_grouping_of_four_tables.ts
+++ lib/queries/four/sort_grouping_of_four_tables.ts
@@ -10,16 +10,16 @@ import {
 export class SortGroupingOfFourTables<T1, T2, T3, T4, K extends ValueRecord> {
     constructor(private readonly statement: GroupSelectStatement) {}
 
-    thenBy(sortBy: (key: K, first: AggregatableTable<T1>, second: AggregatableTable<T2>, third: AggregatableTable<T3>, count: () => Count) => K | Max | Min | Avg | Sum | Count): SortGroupingOfFourTables<T1, T2, T3, T4, K> {
+    thenBy(sortBy: (key: K, first: AggregatableTable<T1>, second: AggregatableTable<T2>, third: AggregatableTable<T3>, fourth: AggregatableTable<T4>, count: () => Count) => K | Max | Min | Avg | Sum | Count): SortGroupingOfFourTables<T1, T2, T3, T4, K> {
         return new SortGroupingOfFourTables(addAscendingGroupOrder(this.statement, sortBy))
     }
 
-    thenDescendinglyBy(sortBy: (key: K, first: AggregatableTable<T1>, second: AggregatableTable<T2>, third: AggregatableTable<T3>, count: () => Count) => K | Max | Min | Avg | Sum | Count): SortGroupingOfFourTables<T1, T2, T3, T4, K> {
+    thenDescendinglyBy(sortBy: (key: K, first: AggregatableTable<T1>, second: AggregatableTable<T2>, third: AggregatableTable<T3>, fourth: AggregatableTable<T4>, count: () => Count) => K | Max | Min | Avg | Sum | Count): SortGroupingOfFourTables<T1, T2, T3, T4, K> {
         return new SortGroupingOfFourTables(addDescendinGroupOrder(this.statement, sortBy))
     }
 
     aggregate<A extends GroupAggregationRecord<K>>(
-        aggregation: (key: K, first: AggregatableTable<T1>, second: AggregatableTable<T2>, third: AggregatableTable<T3>, count: () => Count) => EnforceNonEmptyRecord<A> & A): SelectRows<A> {
+        aggregation: (key: K, first: AggregatableTable<T1>, second: AggregatableTable<T2>, third: AggregatableTable<T3>, fourth: AggregatableTable<T4>, count: () => Count) => EnforceNonEmptyRecord<A> & A): SelectRows<A> {
         return aggregateGroups(this.statement, aggregation)
     }
 }
