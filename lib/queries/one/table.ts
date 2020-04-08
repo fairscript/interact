c189b11b3f1f3aab6426959a4bc092ed13f986d7
sekwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-04-08 16:52:54 +0200

    Added all helper functions needed to rewrite the methods in Table<T>.

--- lib/queries/one/table.ts
+++ lib/queries/one/table.ts
@@ -4,38 +4,34 @@ import {GroupTable} from './group_table'
 import {JoinSecondTable} from '../two/join_second_table'
 import {EnforceNonEmptyRecord, TableAggregationRecord, ValueOrNestedValueRecord, ValueRecord} from '../../record'
 import {Value} from '../../value'
-import {parseSorting} from '../../parsing/sorting/sorting_parsing'
-import {parseGetSelection} from '../../parsing/selection/get_selection_parsing'
-import {parseMapSelection} from '../../parsing/selection/map_selection_parsing'
-import {parseGetKey} from '../../parsing/get_key_parsing'
-import {parseJoin} from '../../parsing/join_parsing'
-import {parseMapWithSubquerySelection} from '../../parsing/selection/maps_selection_parsing'
-import {createCountSelection} from '../../parsing/selection/count_selection'
-import {parseSingleTableSelection} from '../../parsing/selection/single_table_selection_parsing'
 import {Subtable} from './subtable'
-import {parseParameterlessFilter} from '../../parsing/filtering/parameterless_filter_parsing'
-import {parseParameterizedFilter} from '../../parsing/filtering/parameterized_filter_parsing'
 import {SelectScalar} from '../selection/select_scalar'
-import {SelectRows} from '../selection/select_rows'
-import {SelectVector} from '../selection/select_vector'
 import {
-    parseAverageSelection,
-    parseMaxSelection,
-    parseMinSelection,
-    parseSumSelection
-} from '../../parsing/selection/aggregate_column_select_parsing'
+    aggregateTables,
+    averageColumn,
+    countRows,
+    getColumn,
+    groupBy,
+    mapTable,
+    mapTableWithSubquery,
+    maximizeColumn,
+    minimizeColumn,
+    SelectRows,
+    selectTable,
+    sumColumn
+} from '../selection/select_rows'
+import {SelectVector} from '../selection/select_vector'
 import {AggregatableTable, Count} from './aggregatable_table'
-import {parseTableAggregationSelection} from '../../parsing/selection/table_aggregation_selection_parsing'
 import {SelectSingleRow} from '../selection/select_single_row'
 import {
     addAscendingOrder,
     addParameterizedFilter,
     addParameterlessFilter,
     Constructor,
-    createEmptySelectStatement, joinTable,
+    createEmptySelectStatement,
+    joinTable,
     SelectStatement
 } from '../../statements/select_statement'
-import {createEmptyGroupSelectStatement} from '../../statements/group_select_statement'
 
 
 export class Table<T> {
@@ -79,89 +75,49 @@ export class Table<T> {
     }
 
     select(): SelectRows<T> {
-        return new SelectRows(
-            {
-                ...this.statement,
-                selection: parseSingleTableSelection(this.constructor)
-            })
+        return selectTable(this.statement, this.constructor)
     }
 
     map<U extends ValueRecord>(f: (table: T) => EnforceNonEmptyRecord<U> & U): SelectRows<U>
     map<S, U extends ValueRecord>(tableInSubquery: Table<S>, f: (s: Subtable<S>, x: T) => EnforceNonEmptyRecord<U> & U): SelectRows<U>
     map<S, U extends ValueRecord>(fOrTableInSubquery: ((table: T) => EnforceNonEmptyRecord<U> & U)|Table<S>, f?: (s: Subtable<S>, x: T) => EnforceNonEmptyRecord<U> & U): SelectRows<U>{
-        const selection = typeof fOrTableInSubquery === 'function'
-            ? parseMapSelection(fOrTableInSubquery)
-            : parseMapWithSubquerySelection(f!, [fOrTableInSubquery.tableName])
-
-        return new SelectRows(
-            {
-                ...this.statement,
-                selection
-            })
+        return typeof fOrTableInSubquery === 'function'
+            ? mapTable(this.statement, fOrTableInSubquery)
+            : mapTableWithSubquery(this.statement, f!, fOrTableInSubquery)
     }
 
     get<U extends Value>(f: (table: T) => U): SelectVector<U> {
-        return new SelectVector(
-            {
-                ...this.statement,
-                selection: parseGetSelection(f)
-            })
+        return getColumn(this.statement, f)
     }
 
     count(): SelectScalar<number> {
-        return new SelectScalar(
-            {
-                ...this.statement,
-                selection: createCountSelection()
-            })
+        return countRows(this.statement)
     }
 
     max<V extends Value>(f: (table: T) => V): SelectScalar<V> {
-        return new SelectScalar(
-            {
-                ...this.statement,
-                selection: parseMaxSelection(f)
-            })
+        return maximizeColumn(this.statement, f)
     }
 
     min<V extends Value>(f: (table: T) => V): SelectScalar<V> {
-        return new SelectScalar(
-            {
-                ...this.statement,
-                selection: parseMinSelection(f)
-            })
-    }
-
-    average<V extends Value>(f: (table: T) => V): SelectScalar<V> {
-        return new SelectScalar(
-            {
-                ...this.statement,
-                selection: parseAverageSelection(f)
-            })
+        return minimizeColumn(this.statement, f)
     }
 
     sum<V extends Value>(f: (table: T) => V): SelectScalar<V> {
-        return new SelectScalar(
-            {
-                ...this.statement,
-                selection: parseSumSelection(f)
-            })
+        return sumColumn(this.statement, f)
     }
 
-    groupBy<K extends ValueRecord>(getKey: (table: T) => EnforceNonEmptyRecord<K> & K): GroupTable<T, K>{
-        return new GroupTable<T, K>(
-            createEmptyGroupSelectStatement(this.statement.tableName, parseGetKey(getKey))
-        )
+    average<V extends Value>(f: (table: T) => V): SelectScalar<V> {
+        return averageColumn(this.statement, f)
     }
 
     aggregate<A extends TableAggregationRecord>(
         aggregation: (table: AggregatableTable<T>, count: () => Count) => EnforceNonEmptyRecord<A> & A): SelectSingleRow<A> {
 
-        return new SelectSingleRow(
-            {
-                ...this.statement,
-                selection: parseTableAggregationSelection(aggregation, 1)
-            })
+        return aggregateTables(this.statement, aggregation, 1)
+    }
+
+    groupBy<K extends ValueRecord>(getKey: (table: T) => EnforceNonEmptyRecord<K> & K): GroupTable<T, K>{
+        return new GroupTable<T, K>(groupBy(this, getKey))
     }
 }
 
